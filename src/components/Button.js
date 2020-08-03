5ebc6b76972899cdaf25f3e16bba289826be8053
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-03 14:31:58 -0700

    build a button with props (base, confirm, danger, onClik, disabled) + run unit tests + confirm visual validation

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 import "components/Button.scss";
+const classNames = require('classnames')
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = "button";
+ 
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+ 
+   if (props.danger) {
+     buttonClass += " button--danger";
+   }
+ 
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
