db7e1d1685220f5252065d0157177b29f9affbb5
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-03 16:22:15 -0700

    style DayListItem, create object dayClass for the classNames function to apply item, --selected, --full (express conditional classes)

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,18 @@
 import React from "react";
+import "components/DayListItem.scss";
+const classNames = require('classnames');
 
+// props: name, selected, spots, set day
 export default function DayListItem(props) {
+  let dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots
+  })
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li
+    className={dayClass}
+    onClick={() => props.setDay(props.name)}
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
