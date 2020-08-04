06b3dc428e87ca2b2986babb74e8e44d85081f4f
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-04 12:33:21 -0700

    build Interviewer Item Component (.js) + style (.scss) + stories (index.js)

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'components/InterviewerListItem.scss'
+const classNames = require('classnames');
+
+/*  InterviewerListItem component takes in the following props:
+id:number - the id of the interviewer
+name:string - the name of the interviewer
+avatar:url - a url to an image of the interviewer
+selected:boolean - to determine if an interview is selected or not
+setInterviewer:function - sets the interviewer upon selection
+*/
+
+export default function InterviewerListItem(props) {
+  let interviewerStyles = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    "interviewers__item": !props.selected
+  })
+
+  let imageStyles = classNames("interviewers__item-image", {
+    "interviewers__item--selected-image": props.selected,
+    "interviewers__item-image": !props.selected
+  })
+
+  return (
+    <li 
+      className={interviewerStyles}
+      onClick={() => props.setInterviewer(props.name)}
+    >
+      <img
+        className={imageStyles}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  )
+}
