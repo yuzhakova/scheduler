df7bd75dcedc5bb3de3fd41f713b1a91a02591e3
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-03 17:37:43 -0700

    add DayList Component (DayList.js) + stories (index.js)

---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import DayListItem from 'components/DayListItem';
+
+// DayList renders a list of DayListItem components
+export default function DayList(props) {
+  const schedule = props.days.map(dayObj => {  
+    return (
+      <DayListItem
+        key={dayObj.id}
+        name={dayObj.name}
+        spots={dayObj.spots}
+        selected={dayObj.name === props.day}
+        setDay={props.setDay} />         
+    )
+  })
+  return <ul>{schedule}</ul>
+}
+
+// The DayList Component Props: 
+// days:Array a list of day objects (each object includes an id, name, and spots)
+// day:String the currently selected day
+// setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
