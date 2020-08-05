76f2fe4c0710bf7ff00f27ce53ff735b71715f13
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-04 21:46:57 -0700

    add more Appointment views + stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import 'components/Appointment/styles.scss';
 
-export default function Appointment() {
+export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">{props.time}</article>
   )
 }
