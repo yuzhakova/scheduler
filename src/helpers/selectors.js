6cb17041bb1ec35f7552f5606149a3d37a735a37
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-05 20:19:04 -0700

    re-commit

---
+++ src/helpers/selectors.js
@@ -0,0 +1,19 @@
+//Go through an appointments object and return an array pf all the nested objects with that id
+const matchAppointments = (appointments, ids) => {
+  const matched = ids.map(id => appointments[id]);
+  return matched;
+}
+
+//Go through a state array with a days object and an appointments object
+//Match the appointments given in the days object to those in the appointments object
+function getAppointmentsForDay(state, day) {
+  let appointmentArr = [];
+  state.days.map(dayObject => {
+    if (dayObject.name === day) {
+      dayObject.appointments.forEach(apptId => appointmentArr.push(apptId))
+    }
+  })
+  return matchAppointments(state.appointments, appointmentArr);
+}
+
+module.exports = { getAppointmentsForDay };
