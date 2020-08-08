64bcafff4021950cdb8fbf3a06c3e041e2dbb78a
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-08 14:24:31 -0700

    update spots remaining

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -22,6 +22,17 @@ export default function useApplicationData() {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     })
   }, [])
+  
+  function findDay(day) {
+    const daysOfWeek = {
+      Monday: 0,
+      Tuesday: 1,
+      Wednesday: 2,
+      Thursday: 3,
+      Friday: 4
+    }
+    return daysOfWeek[day]
+  }
 
   function bookInterview(id, interview) {
 
@@ -35,11 +46,33 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
+    const dayOfWeek = findDay(state.day)
+
+    let day = {
+      ...state.days[dayOfWeek],
+      spots: state.days[dayOfWeek]
+    }
+
+    if (!state.appointments[id].interview) {
+      day = {
+        ...state.days[dayOfWeek],
+        spots: state.days[dayOfWeek].spots - 1
+      } 
+    } else {
+      day = {
+        ...state.days[dayOfWeek],
+        spots: state.days[dayOfWeek].spots
+      } 
+    }
+
+    let days = state.days
+    days[dayOfWeek] = day;
+    
     return axios.put(`http://localhost:8001/api/appointments/${id}`, {interview:interview})
     .then(res => {
-        setState({...state, appointments})
+        setState({...state, appointments, days})
         return res
-      })
+    })
   }
 
   function cancelInterview(id) {
@@ -53,10 +86,20 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     }
+
+    const dayOfWeek = findDay(state.day)
+
+    const day = {
+      ...state.days[dayOfWeek],
+      spots: state.days[dayOfWeek].spots + 1
+    }
+
+    let days = state.days
+    days[dayOfWeek] = day;
     
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
     .then(res => {
-      setState({...state, appointments})
+      setState({...state, appointments, days})
       return res
     })
   }
