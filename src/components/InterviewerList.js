d70cb89874ded671377a66a48ad55a57a79b7858
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-10 21:21:12 -0700

    add tests for Appointment component with some dependencies

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'components/InterviewerList.scss';
 import InterviewerListItem from 'components/InterviewerListItem';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   /*Takes in Three props
---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,87 @@
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
+import React from "react";
+
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
+import { render, cleanup } from "@testing-library/react";
+
+/*
+  We import the component that we are testing
+*/
+import Appointment from "components/Appointment";
+import Confirm from "components/Appointment/Confirm";
+import Empty from "components/Appointment/Empty";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Status from "components/Appointment/Status";
+
+/*
+  A test that renders a React Component
+*/
+
+afterEach(cleanup);
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  it("Confirm renders w/o crashing", () => {
+    render(<Confirm 
+      message="Confirm"
+    />);
+  });
+
+  it("Empty renders w/o crashing", () => {
+    render(<Empty />);
+  });
+
+  it("Error renders w/o crashing", () => {
+    render(<Error 
+      message="Oops"
+    />);
+  });
+
+  it("Form renders w/o crashing", () => {
+    render(<Form 
+      name={interviewer}
+      value={2}
+      interviewers={interviewers}
+    />);
+  });
+
+  it("Header renders w/o crashing", () => {
+    render(<Header />);
+  });
+
+  it("Show renders w/o crashing", () => {
+    render(<Show 
+      student="random"
+      interviewer={interviewer}
+    />);
+  });
+
+  it("Status renders w/o crashing", () => {
+    render(<Status />);
+  });
+});
