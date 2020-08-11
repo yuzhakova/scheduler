4ca7de004533b05464bc306e2d2ffc4994cf25d5
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-10 22:38:19 -0700

    add Form component tests, render with empty name prop, simulate click button fireEvent, render error text. Check call count onSave

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,7 @@ export default function Form(props) {
   //For keeping track of the name
   const [currentName, setName] = useState(props.name || "");
   const [currentInterviewer, setInterviewer] = useState(props.value || null)
+  const [error, setError] = useState("");
 
   //Helper function to clear all fields
   const reset = () => {
@@ -20,7 +21,14 @@ export default function Form(props) {
     reset();
     props.onCancel()
   }
-  console.log(props)
+
+  function validate() {
+    if (currentName === "") {
+      setError("Student name cannot be blank")
+      return;
+    }
+    props.onSave(currentName, currentInterviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -32,20 +40,21 @@ export default function Form(props) {
             type="text"        
             onChange={(event) => setName(event.target.value)}            
             value={currentName}
-            placeholder={currentName ? currentName : "Please enter your name"}
-
+            //placeholder="Enter Student Name"
+            placeholder={currentName ? currentName : "Enter Student Name"}
+            data-testid="student-name-input"
             /*
               This must be a controlled component
             */
           />
-
+        <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList interviewers={props.interviewers} value={currentInterviewer} onChange={(event) => setInterviewer(event)} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onSubmit={event => event.preventDefault()} onClick={event => props.onSave(currentName, currentInterviewer)}>Save</Button>
+          <Button confirm onSubmit={event => event.preventDefault()} onClick={event => validate()}>Save</Button>
         </section>
       </section>
     </main>
