1a178db1908d6232f74ff4391b8ed4d6aee7c414
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-08-10 21:25:15 -0700

    correct to reverse cleanup for Application.test.js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,12 +7,14 @@ import React from "react";
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 /*
   We import the component that we are testing
 */
 import Application from "components/Application";
+
+afterEach(cleanup);
 
 /*
   A test that renders a React Component
